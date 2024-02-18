e07997ccf4ab8e5def8ab0b8a5d8a0bfbc29a677
RDCJ <1175024349@qq.com>
2024-02-18 16:13:16 +0800

    游戏icon，主界面退出按钮

--- Assets/Scripts/Gaming/Home.cs
+++ Assets/Scripts/Gaming/Home.cs
@@ -12,6 +12,7 @@ public class Home : MonoBehaviour
 {
     Button play_btn;
     Button editor_btn;
+    Button exit_btn;
 
     public GameObject game_mgr;
     public GameObject composition_editor;
@@ -24,6 +25,7 @@ public class Home : MonoBehaviour
         GameConst.gameCFG = Resources.Load<GameCFG>("GameCFG");
         play_btn = transform.Find("play_btn").GetComponent<Button>();
         editor_btn = transform.Find("editor_btn").GetComponent<Button>();
+        exit_btn = transform.Find("exit_btn").GetComponent<Button>();
         play_btn.onClick.AddListener(() => {
             //Instantiate(game_mgr, transform.parent);
             Instantiate(music_select, transform.parent);
@@ -32,10 +34,16 @@ public class Home : MonoBehaviour
         editor_btn.onClick.AddListener(() => {
             CompositionEditor.Instance.Open();
         });
-        if (Application.platform == RuntimePlatform.Android)
+
+        exit_btn.onClick.AddListener(() =>
         {
-            editor_btn.gameObject.SetActive(false);
-        }
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        });
+
         CheckExtractResource();
     }
 
