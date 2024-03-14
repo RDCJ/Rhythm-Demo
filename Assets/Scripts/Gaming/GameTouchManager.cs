4a1a32c465812889396a3752a6fcb95802ddec45
RDCJ <1175024349@qq.com>
2024-03-14 10:35:58 +0800

    1

--- Assets/Scripts/Gaming/GameTouchManager.cs
+++ Assets/Scripts/Gaming/GameTouchManager.cs
@@ -25,69 +25,102 @@ public class TouchEvent
     }
 }
 
-public delegate void TouchEventDelegate(TouchEvent event_data);
+public delegate void TouchEventDelegate(Touch event_data);
 
 public class GameTouchManager : MonoBehaviour
 {
-    public bool PrintLog;
-    bool is_finger_down = false;
-    Vector2 down_position;
-    Vector2 up_position;
-    float up_time;
-    float down_time;
-
-    private Dictionary<TouchEventType, TouchEventDelegate> event_handlers;
-
-/*    private void Start()
+    /*    public bool PrintLog;
+        bool is_finger_down = false;
+        Vector2 down_position;
+        Vector2 up_position;
+        float up_time;
+        float down_time;*/
+    #region Singleton
+    private GameTouchManager() { }
+    private static GameTouchManager instance;
+    public static GameTouchManager Instance
     {
-        AddListener((SlideEvent event_data) =>
+        get
         {
-            if (PrintLog)
-                Debug.Log("OnSlide: " + event_data.down_position + "->" + event_data.up_position);
-        });
+            return instance;
+        }
+    }
+    #endregion
 
-        AddListener((DownEvent event_data) =>
+    private Dictionary<TouchPhase, TouchEventDelegate> event_handlers;
+
+    /*    private void Start()
         {
-            if (PrintLog)
-                Debug.Log("OnDown: " + event_data.down_position);
-        });
-    }*/
+            AddListener((SlideEvent event_data) =>
+            {
+                if (PrintLog)
+                    Debug.Log("OnSlide: " + event_data.down_position + "->" + event_data.up_position);
+            });
+
+            AddListener((DownEvent event_data) =>
+            {
+                if (PrintLog)
+                    Debug.Log("OnDown: " + event_data.down_position);
+            });
+        }*/
 
     // Update is called once per frame
+    public void Reset()
+    {
+        event_handlers = new Dictionary<TouchPhase, TouchEventDelegate>();
+    }
+
+    private void Awake()
+    {
+        instance = this;
+        Reset();
+    }
+
     void Update()
     {
         if (Input.touches.Length > 0)
         {
-            Touch touch = Input.touches[0];
-            if (touch.phase == TouchPhase.Began)
+            foreach (Touch touch in Input.touches)
             {
-                is_finger_down = true;
-                down_position = touch.position;
-                down_time = Time.time;
-/*                var new_event = new DownEvent(touch.fingerId, down_position, down_time);
-                onDown?.Invoke(new_event);*/
-            }
-            else if (touch.phase == TouchPhase.Ended)
-            {
-                if (is_finger_down)
+                if (event_handlers.ContainsKey(touch.phase))
+                    event_handlers[touch.phase]?.Invoke(touch);
+/*                touch.
+                if (touch.phase == TouchPhase.Began)
                 {
-                    is_finger_down = false;
-                    up_position = touch.position;
-                    up_time = Time.time;
-/*                    var new_event = new SlideEvent(down_position, up_position, down_time, up_time);
-                    onSlide?.Invoke(new_event);*/
+                    is_finger_down = true;
+                    down_position = touch.position;
+                    down_time = Time.time;
+                    var new_event = new TouchEvent(touch.fingerId, touch.position, Time.time, TouchEventType.Down);
+                    *//*                var new_event = new DownEvent(touch.fingerId, down_position, down_time);
+                                    onDown?.Invoke(new_event);*//*
+                    event_handlers[]
                 }
+                else if (touch.phase == TouchPhase.Ended)
+                {
+                    if (is_finger_down)
+                    {
+                        is_finger_down = false;
+                        up_position = touch.position;
+                        up_time = Time.time;
+                        *//*                    var new_event = new SlideEvent(down_position, up_position, down_time, up_time);
+                                            onSlide?.Invoke(new_event);*//*
+                    }
+                }
+                TouchPhase.*/
             }
         }
     }
 
-    public void AddListener(TouchEventType type, TouchEventDelegate listener)
+    public void AddListener(TouchPhase type, TouchEventDelegate listener)
     {
+        if (!event_handlers.ContainsKey(type))
+            event_handlers.Add(type, null);
         event_handlers[type] += listener;
     }
 
-    public void RemoveListener(TouchEventType type, TouchEventDelegate listener)
+    public void RemoveListener(TouchPhase type, TouchEventDelegate listener)
     {
+        if (!event_handlers.ContainsKey(type)) return;
         event_handlers[type] -= listener;
     }
 }
--- Assets/Scripts/Test/Test.cs
+++ Assets/Scripts/Test/Test.cs
@@ -10,21 +10,18 @@ namespace Test
 {
     public class Test : MonoBehaviour
     {
-
-
-
         // Start is called before the first frame update
         void Start()
         {
-            HashSet<int> int_set = new();
-            for (int i=0; i<10; i++)
-                int_set.Add(i);
-            var itor = int_set.GetEnumerator();
-            itor.MoveNext();
-            int_set.Remove(itor.Current);
-            Debug.Log(itor.Current);
-            /*            while (itor.MoveNext())
-                            Debug.Log(itor.Current);*/
+            GameTouchManager.Instance.AddListener(TouchPhase.Began, (Touch touch) =>{
+                Debug.Log($"[{this.gameObject.name}] Touch.Began " + touch.position);
+            
+            });
+
+            GameTouchManager.Instance.AddListener(TouchPhase.Ended, (Touch touch) =>
+            {
+                Debug.Log($"[{this.gameObject.name}] Touch.Ended " + touch.position);
+            });
         }
 
         // Update is called once per frame
